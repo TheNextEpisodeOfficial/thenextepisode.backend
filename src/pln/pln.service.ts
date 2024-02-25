c3d87374a013f1bcbb9dae7cac0087d7b6fff0bd
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2024-02-25 23:22:01 +0900

    refactor: srchpln error exception

--- src/pln/pln.service.ts
+++ src/pln/pln.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { PlnEntity } from "@src/pln/entities/pln.entity";
 
@@ -48,13 +48,13 @@ export class PlnService {
     const queryBuilder = this.plnRepository.createQueryBuilder();
     const queryBuildOpts: Partial<SrchPlnDto> = {};
     
+    if (srchPlnDto.plnNm) queryBuildOpts.plnNm = Like(`%${srchPlnDto.plnNm}%`);
     if (srchPlnDto.id) queryBuildOpts.id = srchPlnDto.id;
     if (srchPlnDto.plnTypeCd) queryBuildOpts.plnTypeCd = srchPlnDto.plnTypeCd;
     if (srchPlnDto.plnStTm) queryBuildOpts.plnStTm = srchPlnDto.plnStTm;
     if (srchPlnDto.plnEndTm) queryBuildOpts.plnEndTm = srchPlnDto.plnEndTm;
     if (srchPlnDto.rRatedYn) queryBuildOpts.rRatedYn = srchPlnDto.rRatedYn;
     if (srchPlnDto.delYn) queryBuildOpts.delYn = srchPlnDto.delYn;
-    if (srchPlnDto.plnNm) queryBuildOpts.plnNm = Like(`%${srchPlnDto.plnNm}%`);
     if (srchPlnDto.plnLctnNm) queryBuildOpts.plnLctnNm = Like(`%${srchPlnDto.plnLctnNm}%`);
 
     if (srchPlnDto.plnSrchStDt) {
@@ -71,13 +71,30 @@ export class PlnService {
     queryBuilder.andWhere(queryBuildOpts); //where절 종료
 
     if(srchPlnDto.orderBy) {
-      let order: [string, 'DESC' | 'ASC'] = srchPlnDto.orderBy.split(',')
-      queryBuilder.orderBy(order[0], order[1]);
+      let order: [string, 'DESC' | 'ASC'] = srchPlnDto.orderBy.split(',');
+      if(order.length < 2) {
+        throw new HttpException('orderBy의 파라미터는 <컬럼명,정렬방향>의 양식으로 입력해주세요', HttpStatus.BAD_REQUEST)
+      }
+      let orderColumn = order[0].trim();
+      let orderValue = order[1].trim() as 'DESC' | 'ASC';
+      if(!orderValue || (orderValue != 'DESC' && orderValue != 'ASC')) {
+        throw new HttpException('orderBy 두번째 파라미터는 DESC 혹은 ASC여야 합니다.', HttpStatus.BAD_REQUEST)
+      }
+      queryBuilder.orderBy(orderColumn, orderValue);
     } else {
       queryBuilder.orderBy('createStmp', 'DESC');
     }
 
-    return paginate<PlnEntity>(queryBuilder, srchPlnDto);
+    try {
+      return paginate<PlnEntity>(queryBuilder, srchPlnDto);
+    } catch(error) {
+      throw new HttpException({
+        status: HttpStatus.FORBIDDEN,
+        error: '플랜 검색중 오류가 발생했습니다.',
+      }, HttpStatus.FORBIDDEN, {
+        cause: error
+      });
+    }
   }
   
 }
