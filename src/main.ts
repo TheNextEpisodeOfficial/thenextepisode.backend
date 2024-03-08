5a6f83693eb97cbac5f719a1f742dba6b20f9186
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2024-03-08 17:22:07 +0900

    feat: ascii banner

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,30 @@ import { AppModule } from "./app.module";
 import { setupSwagger } from "./util/swagger";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import express from 'express';
+import express from "express";
 import { join } from "path";
 declare const module: any;
 
 async function bootstrap() {
+  console.log(
+    `
+    ▄▄▄▄    ██▀███   ██▓▓█████▄   ▄████ ▓█████ 
+    ▓█████▄ ▓██ ▒ ██▒▓██▒▒██▀ ██▌ ██▒ ▀█▒▓█   ▀ 
+    ▒██▒ ▄██▓██ ░▄█ ▒▒██▒░██   █▌▒██░▄▄▄░▒███   
+    ▒██░█▀  ▒██▀▀█▄  ░██░░▓█▄   ▌░▓█  ██▓▒▓█  ▄ 
+    ░▓█  ▀█▓░██▓ ▒██▒░██░░▒████▓ ░▒▓███▀▒░▒████▒
+    ░▒▓███▀▒░ ▒▓ ░▒▓░░▓   ▒▒▓  ▒  ░▒   ▒ ░░ ▒░ ░
+    ▒░▒   ░   ░▒ ░ ▒░ ▒ ░ ░ ▒  ▒   ░   ░  ░ ░  ░
+     ░    ░   ░░   ░  ▒ ░ ░ ░  ░ ░ ░   ░    ░   
+     ░         ░      ░     ░          ░    ░  ░
+          ░               ░                     
+    `
+  );
+
   const app = await NestFactory.create(AppModule);
   app.use(cors());
   app.use(cookieParser());
-  app.use('/public', express.static(join(process.cwd(), 'src', 'public')));
+  app.use("/public", express.static(join(process.cwd(), "src", "public")));
 
   app.enableCors({
     origin: true,
--- src/pln/pln.service.ts
+++ src/pln/pln.service.ts
@@ -4,7 +4,7 @@ import { PlnEntity } from "@src/pln/entities/pln.entity";
 
 import { InsertResult, LessThan, Like, MoreThan, Repository } from "typeorm";
 import { SrchPlnDto, UpsertPlanDto } from "./dtos/pln.dto";
-import { IPaginationOptions, Pagination, paginate } from "nestjs-typeorm-paginate";
+import { Pagination, paginate } from "nestjs-typeorm-paginate";
 
 @Injectable()
 export class PlnService {
@@ -47,7 +47,7 @@ export class PlnService {
   async srchPln(srchPlnDto: SrchPlnDto): Promise<Pagination<PlnEntity>> {
     const queryBuilder = this.plnRepository.createQueryBuilder();
     const queryBuildOpts: Partial<SrchPlnDto> = {};
-    
+
     if (srchPlnDto.plnNm) queryBuildOpts.plnNm = Like(`%${srchPlnDto.plnNm}%`);
     if (srchPlnDto.id) queryBuildOpts.id = srchPlnDto.id;
     if (srchPlnDto.plnTypeCd) queryBuildOpts.plnTypeCd = srchPlnDto.plnTypeCd;
@@ -55,46 +55,56 @@ export class PlnService {
     if (srchPlnDto.plnEndTm) queryBuildOpts.plnEndTm = srchPlnDto.plnEndTm;
     if (srchPlnDto.rRatedYn) queryBuildOpts.rRatedYn = srchPlnDto.rRatedYn;
     if (srchPlnDto.delYn) queryBuildOpts.delYn = srchPlnDto.delYn;
-    if (srchPlnDto.plnLctnNm) queryBuildOpts.plnLctnNm = Like(`%${srchPlnDto.plnLctnNm}%`);
+    if (srchPlnDto.plnLctnNm)
+      queryBuildOpts.plnLctnNm = Like(`%${srchPlnDto.plnLctnNm}%`);
 
     if (srchPlnDto.plnSrchStDt) {
       queryBuilder.andWhere({
-        plnDt: MoreThan(srchPlnDto.plnSrchStDt)
-      })
-    };
+        plnDt: MoreThan(srchPlnDto.plnSrchStDt),
+      });
+    }
     if (srchPlnDto.plnSrchEndDt) {
       queryBuilder.andWhere({
-        plnDt: LessThan(srchPlnDto.plnSrchEndDt)
-      })
-    };
+        plnDt: LessThan(srchPlnDto.plnSrchEndDt),
+      });
+    }
 
     queryBuilder.andWhere(queryBuildOpts); //where절 종료
 
-    if(srchPlnDto.orderBy) {
-      let order: [string, 'DESC' | 'ASC'] = srchPlnDto.orderBy.split(',');
-      if(order.length < 2) {
-        throw new HttpException('orderBy의 파라미터는 <컬럼명,정렬방향>의 양식으로 입력해주세요', HttpStatus.BAD_REQUEST)
+    if (srchPlnDto.orderBy) {
+      let order: [string, "DESC" | "ASC"] = srchPlnDto.orderBy.split(",");
+      if (order.length < 2) {
+        throw new HttpException(
+          "orderBy의 파라미터는 <컬럼명,정렬방향>의 양식으로 입력해주세요",
+          HttpStatus.BAD_REQUEST
+        );
       }
       let orderColumn = order[0].trim();
-      let orderValue = order[1].trim() as 'DESC' | 'ASC';
-      if(!orderValue || (orderValue != 'DESC' && orderValue != 'ASC')) {
-        throw new HttpException('orderBy 두번째 파라미터는 DESC 혹은 ASC여야 합니다.', HttpStatus.BAD_REQUEST)
+      let orderValue = order[1].trim() as "DESC" | "ASC";
+      if (!orderValue || (orderValue != "DESC" && orderValue != "ASC")) {
+        throw new HttpException(
+          "orderBy 두번째 파라미터는 DESC 혹은 ASC여야 합니다.",
+          HttpStatus.BAD_REQUEST
+        );
       }
       queryBuilder.orderBy(orderColumn, orderValue);
     } else {
-      queryBuilder.orderBy('createStmp', 'DESC');
+      queryBuilder.orderBy("createStmp", "DESC");
     }
 
     try {
       return paginate<PlnEntity>(queryBuilder, srchPlnDto);
-    } catch(error) {
-      throw new HttpException({
-        status: HttpStatus.FORBIDDEN,
-        error: '플랜 검색중 오류가 발생했습니다.',
-      }, HttpStatus.FORBIDDEN, {
-        cause: error
-      });
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.FORBIDDEN,
+          error: "플랜 검색중 오류가 발생했습니다.",
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: error,
+        }
+      );
     }
   }
-  
 }
