ea32f6ff05c252e7d7f3393ba81303e2facf7c21
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2024-04-07 22:26:20 +0900

    fix: s3 file entity

--- src/s3file/entities/file.entity.ts
+++ src/s3file/entities/file.entity.ts
@@ -3,17 +3,17 @@ import { CommonEntity } from "../../config/entities/common.entity";
 
 @Entity("s3_file")
 export class FileEntity extends CommonEntity {
-  @Column({ type: "varchar", length: 1000, comment: "url" })
-  url;
+  @Column({ type: "varchar", length: 1000, comment: "파일명.확장자" })
+  fileNm;
 
   @Column({
     type: "varchar",
-    length: 100,
+    length: 36,
     comment: "파일그룹아이디",
     nullable: true,
   })
   fileGrpId;
 
-  @Column({ type: "varchar", length: 100, comment: "파일 종류 코드" })
+  @Column({ type: "varchar", length: 8, comment: "파일 종류 코드" })
   fileTypeCd;
 }
