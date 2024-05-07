f132fc9059f811fd0f6c90412a7fa76f1f9a0ba2
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2024-05-08 05:18:31 +0900

    fix: set data type for celeb entity

--- src/celeb/entities/celeb.entity.ts
+++ src/celeb/entities/celeb.entity.ts
@@ -1,15 +1,19 @@
 import { MbrEntity } from "@src/mbr/entities/mbr.entity";
-import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne } from "typeorm";
 import { CommonEntity } from "../../config/entities/common.entity";
+import { BttlOptRoleEntity } from "@src/bttlOptRole/entities/bttlOptRole.entity";
 
 @Entity("celeb")
 export class CelebEntity extends CommonEntity {
   @Column({ type: "varchar", length: 100, comment: "셀럽 본명" })
   celebNm;
 
-  @Column({ type: "varchar", length: 100, comment: "셀럽 닉네임" })
+  @Column({ type: "varchar", length: 30, comment: "셀럽 닉네임" })
   celebNckNm;
 
-  @Column({ type: "varchar", length: 100, comment: "주요 역할 코드" })
+  @Column({ type: "varchar", length: 4, comment: "주요 역할 코드" })
   celebTypeCd;
+
+  @Column({ type: "varchar", length: 4, comment: "주요 장르 코드" })
+  celebMnJnr;
 }
