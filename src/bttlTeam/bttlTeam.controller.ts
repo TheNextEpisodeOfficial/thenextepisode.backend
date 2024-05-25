2ee00545888e38d8bceb7b357f9bbeb08afefd8f
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2024-05-26 07:24:17 +0900

    feat: 참가, 입장 신청, 결제 insert 아키텍쳐 준비

---
+++ src/bttlTeam/bttlTeam.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param, Query } from "@nestjs/common";
+import {
+  ApiTags,
+} from "@nestjs/swagger";
+
+/**
+ * PlnController : 플랜 테이블을 관리한다
+ */
+@Controller("/bttlTeam")
+@ApiTags("bttlTeam")
+export class BttlTeamController {
+  constructor(
+    
+  ) {}
+
+
+}
