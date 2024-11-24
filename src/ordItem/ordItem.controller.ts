8fd450113bd1bef6a3453cfd92ae3168f24171a3
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2024-11-24 18:36:36 +0900

    fix: ordItem 컨트롤러명 수정

--- src/ordItem/ordItem.controller.ts
+++ src/ordItem/ordItem.controller.ts
@@ -5,8 +5,8 @@ import { OrdItemService } from "./ordItem.service";
 /**
  * OrdItemController : 주문상품 API를 관리한다
  */
-@Controller("/ordPayment")
-@ApiTags("OrdPayment")
+@Controller("/ordItem")
+@ApiTags("OrdItem")
 export class OrdItemController {
   constructor(private readonly ordItemService: OrdItemService) {}
 }
